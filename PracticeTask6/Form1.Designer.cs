a2322b35fff0caa398c91f865481f0221e0d3321
skjfdnv <skjfdnv@yandex.ru>
2020-05-30 17:29:55 +0500

    Upd

--- PracticeTask6/Form1.Designer.cs
+++ PracticeTask6/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.a1_input = new System.Windows.Forms.TextBox();
+            this.a2_input = new System.Windows.Forms.TextBox();
+            this.a3_input = new System.Windows.Forms.TextBox();
+            this.m_input = new System.Windows.Forms.TextBox();
+            this.Sequence_output = new System.Windows.Forms.Label();
+            this.Equals_output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // a1_input
+            // 
+            this.a1_input.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a1_input.Location = new System.Drawing.Point(173, 33);
+            this.a1_input.Name = "a1_input";
+            this.a1_input.Size = new System.Drawing.Size(110, 41);
+            this.a1_input.TabIndex = 0;
+            this.a1_input.TextChanged += new System.EventHandler(this.a1_input_TextChanged);
+            this.a1_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // a2_input
+            // 
+            this.a2_input.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a2_input.Location = new System.Drawing.Point(173, 95);
+            this.a2_input.Name = "a2_input";
+            this.a2_input.Size = new System.Drawing.Size(110, 41);
+            this.a2_input.TabIndex = 1;
+            this.a2_input.TextChanged += new System.EventHandler(this.a2_input_TextChanged);
+            this.a2_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // a3_input
+            // 
+            this.a3_input.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a3_input.Location = new System.Drawing.Point(173, 159);
+            this.a3_input.Name = "a3_input";
+            this.a3_input.Size = new System.Drawing.Size(110, 41);
+            this.a3_input.TabIndex = 2;
+            this.a3_input.TextChanged += new System.EventHandler(this.a3_input_TextChanged);
+            this.a3_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // m_input
+            // 
+            this.m_input.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_input.Location = new System.Drawing.Point(173, 225);
+            this.m_input.Name = "m_input";
+            this.m_input.Size = new System.Drawing.Size(110, 41);
+            this.m_input.TabIndex = 3;
+            this.m_input.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.m_input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            // 
+            // Sequence_output
+            // 
+            this.Sequence_output.AutoSize = true;
+            this.Sequence_output.BackColor = System.Drawing.Color.Transparent;
+            this.Sequence_output.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sequence_output.Location = new System.Drawing.Point(43, 410);
+            this.Sequence_output.Name = "Sequence_output";
+            this.Sequence_output.Size = new System.Drawing.Size(0, 33);
+            this.Sequence_output.TabIndex = 4;
+            // 
+            // Equals_output
+            // 
+            this.Equals_output.AutoSize = true;
+            this.Equals_output.BackColor = System.Drawing.Color.Transparent;
+            this.Equals_output.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Equals_output.Location = new System.Drawing.Point(43, 458);
+            this.Equals_output.Name = "Equals_output";
+            this.Equals_output.Size = new System.Drawing.Size(0, 33);
+            this.Equals_output.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.BackgroundImage = global::PracticeTask6.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(598, 636);
+            this.Controls.Add(this.Equals_output);
+            this.Controls.Add(this.Sequence_output);
+            this.Controls.Add(this.m_input);
+            this.Controls.Add(this.a3_input);
+            this.Controls.Add(this.a2_input);
+            this.Controls.Add(this.a1_input);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1181, 687);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox a1_input;
+        private System.Windows.Forms.TextBox a2_input;
+        private System.Windows.Forms.TextBox a3_input;
+        private System.Windows.Forms.TextBox m_input;
+        private System.Windows.Forms.Label Sequence_output;
+        private System.Windows.Forms.Label Equals_output;
     }
 }
 
--- PracticeTask6/Form1.cs
+++ PracticeTask6/Form1.cs
@@ -16,5 +16,166 @@ namespace PracticeTask6
         {
             InitializeComponent();
         }
+        static double m;
+        const int size = 3;
+        static double[] buf = new double[size];
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                m_input.Text = m_input.Text.Replace('.', ',');
+                if (double.TryParse(m_input.Text, out m))
+                {
+                    if (Math.Abs(buf[2]) <= Math.Pow(10, 10))
+                    {
+                        if (!double.TryParse(a1_input.Text, out buf[0]))
+                            a1_input.Focus();
+                        else if (!double.TryParse(a2_input.Text, out buf[1]))
+                            a2_input.Focus();
+                        else if (!double.TryParse(a3_input.Text, out buf[2]))
+                            a3_input.Focus();
+                        else Recursion(counter: 0);
+                    }
+                    else MessageBox.Show("Введите число, по модулю не превышающее 10^10!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else MessageBox.Show("Некорректный ввод числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        string result = string.Empty;
+        void Recursion(int counter)
+        {
+            if (counter < size)
+            {
+                if (buf[counter] < m)
+                {
+                    result += buf[counter].ToString() + " ";
+                    counter++;
+                    Recursion(counter);
+                }
+                else
+                {
+                    /*if (buf[counter] == m)*/ result += buf[counter].ToString() + " ";
+                    Sequence_output.Text = result;
+                    if (buf[counter] == m) Equals_output.Text = "An равно m";
+                    else Equals_output.Text = "An не равно m";
+                }
+            }
+            else
+            {
+                double digit = buf[size - 1] * buf[size - 2] + buf[size - 3];
+                if (digit < m)
+                {
+                    result += digit.ToString() + " ";
+                    double temp = buf[size - 1];
+                    buf[size - 1] = digit;
+                    buf[size - 3] = buf[size - 2];
+                    buf[size - 2] = temp;
+                    counter++;
+                    Recursion(counter);
+                }
+                else
+                {
+                    /*if (digit == m)*/ result += digit.ToString() + " ";
+                    Sequence_output.Text = result;
+                    if (digit == m) Equals_output.Text = "An равно m";
+                    else Equals_output.Text = "An не равно m";
+                }
+            }
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            result = string.Empty;
+            Sequence_output.Text = string.Empty;
+            Equals_output.Text = string.Empty;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                a1_input.Text = a1_input.Text.Replace('.', ',');
+                if (double.TryParse(a1_input.Text, out buf[0]))
+                {
+                    if (Math.Abs(buf[0]) <= Math.Pow(10, 10))
+                    {
+                        if (double.TryParse(a2_input.Text, out buf[1]) && double.TryParse(a3_input.Text, out buf[2]) && double.TryParse(m_input.Text, out m))
+                        {
+                            Sequence_output.Text = string.Empty;
+                            Equals_output.Text = string.Empty;
+                            Recursion(counter: 0);
+                        }
+                        else a2_input.Focus();
+                    }
+                    else MessageBox.Show("Введите число, по модулю не превышающее 10^10!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else MessageBox.Show("Некорректный ввод числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                a2_input.Text = a2_input.Text.Replace('.', ',');
+                if (double.TryParse(a2_input.Text, out buf[1]))
+                {
+                    if (Math.Abs(buf[1]) <= Math.Pow(10, 10))
+                    {
+                        if (double.TryParse(a1_input.Text, out buf[0]) && double.TryParse(a3_input.Text, out buf[2]) && double.TryParse(m_input.Text, out m))
+                        {
+                            Sequence_output.Text = string.Empty;
+                            Equals_output.Text = string.Empty;
+                            Recursion(counter: 0);
+                        }
+                        else a3_input.Focus();
+                    }
+                    else MessageBox.Show("Введите число, по модулю не превышающее 10^10!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else MessageBox.Show("Некорректный ввод числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                a3_input.Text = a3_input.Text.Replace('.', ',');
+                if (double.TryParse(a3_input.Text, out buf[2]))
+                {
+                    if (Math.Abs(buf[2]) <= Math.Pow(10, 10))
+                    {
+                        if (double.TryParse(a1_input.Text, out buf[0]) && double.TryParse(a2_input.Text, out buf[1]) && double.TryParse(m_input.Text, out m))
+                        {
+                            Sequence_output.Text = string.Empty;
+                            Equals_output.Text = string.Empty;
+                            Recursion(counter: 0);
+                        }
+                        else m_input.Focus();
+                    }
+                    else MessageBox.Show("Введите число, по модулю не превышающее 10^10!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else MessageBox.Show("Некорректный ввод числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a1_input_TextChanged(object sender, EventArgs e)
+        {
+            result = string.Empty;
+        }
+
+        private void a2_input_TextChanged(object sender, EventArgs e)
+        {
+            result = string.Empty;
+        }
+
+        private void a3_input_TextChanged(object sender, EventArgs e)
+        {
+            result = string.Empty;
+        }
     }
 }
