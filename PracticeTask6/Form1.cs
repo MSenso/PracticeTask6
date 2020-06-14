8e22e808635654524c968a2fd4f80634de4cbee5
skjfdnv <skjfdnv@yandex.ru>
2020-06-14 22:19:23 +0500

    Upd

--- PracticeTask6/Form1.cs
+++ PracticeTask6/Form1.cs
@@ -16,44 +16,31 @@ namespace PracticeTask6
         {
             InitializeComponent();
         }
-        static double m;
+        public double m;
         const int size = 3;
-        static double[] buf = new double[size];
+        public double[] buf = new double[size]; // Три введенных числа
         string result = string.Empty;
-        double Max()
-        {
-            if (buf[0] > buf[1])
-            {
-                if (buf[0] > buf[2]) return buf[0];
-                else return buf[2];
-            }
-            else if (buf[1] > buf[2]) return buf[1];
-            else return buf[2];
-        }
         private void textBox4_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter) // Нажат энтер
             {
-                m_input.Text = m_input.Text.Replace('.', ',');
-                if (double.TryParse(m_input.Text, out m))
+                m_input.Text = m_input.Text.Replace('.', ','); // Замена точки на запятую для корректного парса строки
+                if (double.TryParse(m_input.Text, out m)) // Введено вещественное число
                 {
-                    if (Math.Abs(buf[2]) <= Math.Pow(10, 10))
+                    if (Math.Abs(m) <= Math.Pow(10, 10)) // Введено число, не превосходящее 10 в 10
                     {
-                        if (!double.TryParse(a1_input.Text, out buf[0]))
+                        if (!double.TryParse(a1_input.Text, out buf[0])) // Введено не вещественное число
                             a1_input.Focus();
-                        else if (!double.TryParse(a2_input.Text, out buf[1]))
+                        else if (!double.TryParse(a2_input.Text, out buf[1])) // Введено не вещественное число
                             a2_input.Focus();
-                        else if (!double.TryParse(a3_input.Text, out buf[2]))
+                        else if (!double.TryParse(a3_input.Text, out buf[2])) // Введено не вещественное число
                             a3_input.Focus();
                         else
                         {
-                            if (buf[1] == 0 && buf[2] == 0)
-                            {
-                                if (m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                else Calculating();
-                            }
-                            else if ((buf[1] == 0 || buf[2] == 0) && buf[0] == 0 && m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            else Calculating();
+                            // Очистка результатов
+                            Sequence_output.Text = string.Empty;
+                            Equals_output.Text = string.Empty;
+                            Compare_With_M(); // Сравнение введенных а1-а3 с М
                         }
                     }
                     else MessageBox.Show("Введите число, по модулю не превышающее 10^10!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -61,34 +48,39 @@ namespace PracticeTask6
                 else MessageBox.Show("Некорректный ввод числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        double Recursion(int x)
+        double Recursion(int x) // Рекурсия
         {
             if (x < size)
             {
-                return buf[x];
+                return buf[x]; // Возвращается ax
             }
             else
             {
-                double res = Recursion(x - 1) * Recursion(x - 2) + Recursion(x - 3);
+                double res = Recursion(x - 1) * Recursion(x - 2) + Recursion(x - 3); // Подсчет по формуле
                 return res;
             }
         }
-        void Calculating()
+        public void Calculating() // Вычисление последовательности
         {
-            int x = 0;
-            double result_value = 0;
+            int count = 0; // Счетчик элементов
+            int x = 0; // Текущий номер элемента
+            double result_value = 0; // Полученное значение
+            result = string.Empty;
             do
             {
                 result_value = Recursion(x);
-                result += result_value.ToString() + " ";
-                x++;
-            } while (result_value < m);
+                result += result_value.ToString() + " "; // Запись полученного значения в выходную строку
+                x++; // Номер увеличивается
+                count++; // Количество элементов увеличивается
+            } while (result_value < m); // Пока полученное значение меньше M
             Sequence_output.Text = result;
-            if (result_value == m) Equals_output.Text = "An равно m";
-            else Equals_output.Text = "An не равно m";
+            Equals_output.Text = "Количество элементов: " + count + "\n";
+            if (result_value == m) Equals_output.Text += "An равно m";
+            else Equals_output.Text += "An не равно m";
         }
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
+            // Очистка результатов
             result = string.Empty;
             Sequence_output.Text = string.Empty;
             Equals_output.Text = string.Empty;
@@ -96,24 +88,19 @@ namespace PracticeTask6
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter) // Нажат энтер
             {
-                a1_input.Text = a1_input.Text.Replace('.', ',');
-                if (double.TryParse(a1_input.Text, out buf[0]))
+                a1_input.Text = a1_input.Text.Replace('.', ','); // Замена точки на запятую для корректного парса строки
+                if (double.TryParse(a1_input.Text, out buf[0])) // Введено вещественное число
                 {
-                    if (Math.Abs(buf[0]) <= Math.Pow(10, 10))
+                    if (Math.Abs(buf[0]) <= Math.Pow(10, 10)) // Введенное число не превосходит 10 в 10
                     {
-                        if (double.TryParse(a2_input.Text, out buf[1]) && double.TryParse(a3_input.Text, out buf[2]) && double.TryParse(m_input.Text, out m))
+                        if (double.TryParse(a2_input.Text, out buf[1]) && double.TryParse(a3_input.Text, out buf[2]) && double.TryParse(m_input.Text, out m)) // Остальные числа тоже корректно введены
                         {
+                            // Очистка результатов
                             Sequence_output.Text = string.Empty;
                             Equals_output.Text = string.Empty;
-                            if (buf[1] == 0 && buf[2] == 0)
-                            {
-                                if (m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                else Calculating();
-                            }
-                            else if ((buf[1] == 0 || buf[2] == 0) && buf[0] == 0 && m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            else Calculating();
+                            Compare_With_M(); // Сравнение а1-а3 с М
                         }
                         else a2_input.Focus();
                     }
@@ -125,24 +112,19 @@ namespace PracticeTask6
 
         private void textBox2_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter) // Нажат энтер
             {
-                a2_input.Text = a2_input.Text.Replace('.', ',');
-                if (double.TryParse(a2_input.Text, out buf[1]))
+                a2_input.Text = a2_input.Text.Replace('.', ','); // Замена точки на запятую для корректного парса строки
+                if (double.TryParse(a2_input.Text, out buf[1])) // Введено вещественное число
                 {
-                    if (Math.Abs(buf[1]) <= Math.Pow(10, 10))
+                    if (Math.Abs(buf[1]) <= Math.Pow(10, 10)) // Введенное число не превосходит 10 в 10
                     {
-                        if (double.TryParse(a1_input.Text, out buf[0]) && double.TryParse(a3_input.Text, out buf[2]) && double.TryParse(m_input.Text, out m))
+                        if (double.TryParse(a1_input.Text, out buf[0]) && double.TryParse(a3_input.Text, out buf[2]) && double.TryParse(m_input.Text, out m)) // Остальные числа тоже корректно введены
                         {
+                            // Очистка результатов
                             Sequence_output.Text = string.Empty;
                             Equals_output.Text = string.Empty;
-                            if (buf[1] == 0 && buf[2] == 0)
-                            {
-                                if (m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                else Calculating();
-                            }
-                            else if ((buf[1] == 0 || buf[2] == 0) && buf[0] == 0 && m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            else Calculating();
+                            Compare_With_M(); // Сравнение а1-а3 с М
                         }
                         else a3_input.Focus();
                     }
@@ -154,24 +136,19 @@ namespace PracticeTask6
 
         private void textBox3_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter) // Нажат энтер
             {
-                a3_input.Text = a3_input.Text.Replace('.', ',');
-                if (double.TryParse(a3_input.Text, out buf[2]))
+                a3_input.Text = a3_input.Text.Replace('.', ','); // Замена точки на запятую для корректного парса строки
+                if (double.TryParse(a3_input.Text, out buf[2])) // Введено вещественное число
                 {
-                    if (Math.Abs(buf[2]) <= Math.Pow(10, 10))
+                    if (Math.Abs(buf[2]) <= Math.Pow(10, 10)) // Введенное число не превосходит 10 в 10
                     {
-                        if (double.TryParse(a1_input.Text, out buf[0]) && double.TryParse(a2_input.Text, out buf[1]) && double.TryParse(m_input.Text, out m))
+                        if (double.TryParse(a1_input.Text, out buf[0]) && double.TryParse(a2_input.Text, out buf[1]) && double.TryParse(m_input.Text, out m)) // Остальные числа тоже корректно введены
                         {
+                            // Очистка результатов
                             Sequence_output.Text = string.Empty;
                             Equals_output.Text = string.Empty;
-                            if (buf[1] == 0 && buf[2] == 0)
-                            {
-                                if (m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                                else Calculating();
-                            }
-                            else if ((buf[1] == 0 || buf[2] == 0) && buf[0] == 0 && m > Max()) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            else Calculating();
+                            Compare_With_M(); // Сравнение а1-а3 с М
                         }
                         else m_input.Focus();
                     }
@@ -180,7 +157,20 @@ namespace PracticeTask6
                 else MessageBox.Show("Некорректный ввод числа!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
+        void Compare_With_M()
+        {
+            if (buf[1] == 0 && buf[2] == 0) // а2 и а3 равны нулю
+            {
+                if (m > buf[0]) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information); // Последовательность не закончится
+                else Calculating(); // Подсчет
+            }
+            else if (buf[1] == 0 && buf[0] == 0) // а2 и а1 равны нулю
+            {
+                if (m > buf[2]) MessageBox.Show("Члены последовательности всегда будут меньше m!", "Вывод", MessageBoxButtons.OK, MessageBoxIcon.Information); // Последовательность не закончится
+                else Calculating(); // Подсчет
+            }
+            else Calculating(); // Подсчет
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -188,16 +178,19 @@ namespace PracticeTask6
 
         private void a1_input_TextChanged(object sender, EventArgs e)
         {
+            // Очищение результатов
             result = string.Empty;
         }
 
         private void a2_input_TextChanged(object sender, EventArgs e)
         {
+            // Очищение результатов
             result = string.Empty;
         }
 
         private void a3_input_TextChanged(object sender, EventArgs e)
         {
+            // Очищение результатов
             result = string.Empty;
         }
     }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeTask6;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Form1 form = new Form1();
+            form.buf[0] = 1;
+            form.buf[1] = 2;
+            form.buf[2] = 3;
+            form.m = 10;
+            form.Calculating();
+
+            form.buf[0] = 1;
+            form.buf[1] = 2;
+            form.buf[2] = 3;
+            form.m = -10;
+            form.Calculating();
+
+            form.buf[0] = 1;
+            form.buf[1] = 0;
+            form.buf[2] = 1;
+            form.m = 100;
+            form.Calculating();
+
+            form.buf[0] = 0;
+            form.buf[1] = 0;
+            form.buf[2] = 1;
+            form.m = 1;
+            form.Calculating();
+
+            form.buf[0] = 0;
+            form.buf[1] = 0;
+            form.buf[2] = 1;
+            form.m = 0;
+            form.Calculating();
+
+            form.buf[0] = 0;
+            form.buf[1] = 0;
+            form.buf[2] = 0;
+            form.m = 0;
+            form.Calculating();
+
+            form.buf[0] = 1;
+            form.buf[1] = 2;
+            form.buf[2] = 3;
+            form.m = 164;
+            form.Calculating();
+        }
+    }
+}
